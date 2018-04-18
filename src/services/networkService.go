5e6a74554d707a2f40d34b6a2b6fed2c0c9350b4
マサチ <zhangjb42@gmail.com>
2018-04-18 21:08:44 +0800

    GB

--- src/services/networkService.go
+++ src/services/networkService.go
@@ -14,7 +14,7 @@ func NetWorkService(originData interface{}) interface{} {
 
 	for i := 0; i< networkData.Len(); i++ {
 		date := response.MetricDate{}
-		metricData = append(metricData, networkData.Index(i).Elem().FieldByName("Sum").Elem().Float() / 1024 / 1024)
+		metricData = append(metricData, networkData.Index(i).Elem().FieldByName("Sum").Elem().Float() / 1024 / 1024 / 1024)
 		date.Timestamp = networkData.Index(i).Elem().FieldByName("Timestamp").Elem().Interface()
 		//date.Year = networkData.Index(i).Elem().FieldByName("Timestamp").Elem().String()[0:4]
 		//date.Date = strings.Replace(networkData.Index(i).Elem().FieldByName("Timestamp").Elem().String()[5:10], "-", ".", -1)
