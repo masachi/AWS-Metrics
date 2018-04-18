7895a78b0e73b52b9ed810075a5032b96bc122e8
マサチ <zhangjb42@gmail.com>
2018-04-18 20:31:19 +0800

    CORS

--- src/main/main.go
+++ src/main/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"AWS-Metrics/src/router"
 	"AWS-Metrics/src/instance"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := instance.GetInstance()
 	router.Router()
+	e.Use(middleware.CORSWithConfig(middleware.DefaultCORSConfig))
 	e.Logger.Fatal(e.Start(":1323"))
 }
