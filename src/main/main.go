73eb63aa0f76c340314343b42c2444b82e196292
マサチ <zhangjb42@gmail.com>
2018-04-19 15:46:08 +0800

    comma

--- src/main/main.go
+++ src/main/main.go
@@ -14,7 +14,7 @@ func main() {
 		Skipper:      middleware.DefaultSkipper,
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE, echo.OPTIONS},
-		AllowHeaders: []string{"Origin", "No-Cache", "X-Requested-With", "If-Modified-Since", "Pragma", "Last-Modified", "Cache-Control", "Expires", "Content-Type", "X-E4M-With"}
+		AllowHeaders: []string{"Origin", "No-Cache", "X-Requested-With", "If-Modified-Since", "Pragma", "Last-Modified", "Cache-Control", "Expires", "Content-Type", "X-E4M-With"},
 	}))
 	e.Logger.Fatal(e.Start(":1323"))
 }
